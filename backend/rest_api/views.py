7b35e0b0b8734c7e0682d3a9a887f8bef591ef64
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2020-07-05 19:20:03 +0200

    Mate views added, not tested

--- backend/rest_api/views.py
+++ backend/rest_api/views.py
@@ -85,3 +85,35 @@ def user_room_list(request):
     data = RoomOffer.objects.filter(owner=request.user)
     serializer = RoomOfferListSerializer(data, many=True)
     return Response(serializer.data)
+
+
+@api_view(['POST', 'PUT', 'DELETE'])
+def user_mate_detail(request):
+    data = request.data
+    data['owner'] = request.user.id
+
+    if request.method == 'POST':
+        serializer = MateOfferDetailSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'PUT':
+        offer = get_object_or_404(MateOffer,  id=data['id'], owner=data['owner'])
+        serializer = MateOfferDetailSerializer(offer, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=200)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        offer = get_object_or_404(MateOffer,  id=data['id'], owner=data['owner'])
+        offer.delete()
+        return Response(status=200)
+
+@api_view(['GET'])
+def user_mate_list(request):
+    data = MateOffer.objects.filter(owner=request.user)
+    serializer = MateOfferListSerializer(data, many=True)
+    return Response(serializer.data)
