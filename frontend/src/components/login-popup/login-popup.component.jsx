9f516dd853a96b8d40a8f4a2038005afc0431f9b
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2020-07-18 14:27:48 +0200

    Positioned, added font-awesome

--- frontend/src/components/login-popup/login-popup.component.jsx
+++ frontend/src/components/login-popup/login-popup.component.jsx
@@ -2,28 +2,42 @@ import React from 'react';
 import './login-popup.styles.css';
 import { InputAndLabel } from '../input-and-label/input-and-label.component';
 import { PopupButton } from '../popup-button/popup-button.component';
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 
 class LoginPopup extends React.Component {
     constructor(props) {
         super(props);
+        this.state = { visible: true }
     }
 
     render() {
         return (
-            <div className="LoginPopup-container">
-                <div className="LoginPopup-form-wrapper">
-                    <h3>Log in</h3>
-                    <InputAndLabel label="username" />
-                    <InputAndLabel label="password" />
-                    <div className="LoginPopup-links">
-                        <p>No account?</p><p className="LoginPopup-register">Register</p>
-                        <p className="LoginPopup-forgot">Forgot password?</p>
+            <div>
+                {this.state.visible &&
+                    <div className="LoginPopup-container">
+                        <FontAwesomeIcon icon={faTimes} className="LoginPopup-close" onClick={e => {
+                            this.setState({
+                                visible: false
+                            })
+                        }} />
+                        <div className="LoginPopup-form-wrapper">
+                            <h3>Log in</h3>
+                            <InputAndLabel label="username" />
+                            <InputAndLabel label="password" />
+                            <div className="LoginPopup-links">
+                                <p>No account?</p><p className="LoginPopup-register">Register</p>
+                                <p className="LoginPopup-forgot">Forgot password?</p>
+                            </div>
+                            <div className="LoginPopup-button">
+                                <PopupButton content="Log in" />
+                            </div>
+                        </div>
                     </div>
-                    <div className="LoginPopup-button">
-                        <PopupButton content="Log in" />
-                    </div>
-                </div>
+                }
             </div>
+
         )
     }
 }
