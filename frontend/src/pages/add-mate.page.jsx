7c65ffb279e179b38d571c9e5ddea9fc8855b15e
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2020-08-13 15:24:57 +0200

    Final styles

--- frontend/src/pages/add-mate.page.jsx
+++ frontend/src/pages/add-mate.page.jsx
@@ -23,15 +23,11 @@ const SecondRow = styled.div`
 `
 
 const ThirdRow = styled.div`
-    width: 570px;
-    display: flex;
 `
 
 const ForthRow = styled.div`
 `
 
-const FifthRow = styled.div`
-`
 
 // const Features = styled.div`
 //     margin-top: 70px;
@@ -118,9 +114,9 @@ class AddMatePage extends React.Component {
                         data.append('title', values.title);
                         data.append('field_of_study', values.field_of_study);
                         data.append('age', values.age);
-                        data.append('location', 'Plac');
-                        data.append('features', 'peaceful;quiet;gaming;cycling');
-                        data.append('customs', 'no smoking;no partying;wakes up at 11-12;goes to bed 23-24');
+                        data.append('location', values.location);
+                        data.append('features', values.features);
+                        data.append('customs', values.customs);
                         data.append('phone', values.phone);
 
                         axios.post('http://localhost:8000/user_mate_detail/', data, {
@@ -146,32 +142,37 @@ class AddMatePage extends React.Component {
                             <FileInput setFieldValue={props.setFieldValue} />
 
                             <SecondRow>
-                                <SmallInputAndLabel pos="left" label="Age" id="age" name="age" value={props.values.age} onChange={props.handleChange} />
-                                {props.errors.age && <Feedback>{props.errors.age}</Feedback>}
-                                <SmallInputAndLabel pos="left" label="Field of study" id="field_of_study" name="field_of_study" value={props.values.field_of_study} onChange={props.handleChange} />
-                                {props.errors.field_of_study && <Feedback>{props.errors.field_of_study}</Feedback>}
-                                <SmallInputAndLabel pos="left" label="Phone" id="phone" name="phone" value={props.values.phone} onChange={props.handleChange} />
-                                {props.errors.phone && <Feedback>{props.errors.phone}</Feedback>}
-                                <SmallInputAndLabel pos="left" label="Location" id="location" name="location" value={props.values.location} onChange={props.handleChange} />
-                                {props.errors.location && <Feedback>{props.errors.location}</Feedback>}
+                                <div>
+                                    <SmallInputAndLabel pos="left" label="Age" id="age" name="age" value={props.values.age} onChange={props.handleChange} />
+                                    {props.errors.age && <Feedback>{props.errors.age}</Feedback>}
+                                </div>
+                                <div>
+
+                                    <SmallInputAndLabel pos="left" label="Field of study" id="field_of_study" name="field_of_study" value={props.values.field_of_study} onChange={props.handleChange} />
+                                    {props.errors.field_of_study && <Feedback>{props.errors.field_of_study}</Feedback>}
+                                </div>
+                                <div>
+                                    <SmallInputAndLabel pos="left" label="Phone" id="phone" name="phone" value={props.values.phone} onChange={props.handleChange} />
+                                    {props.errors.phone && <Feedback>{props.errors.phone}</Feedback>}
+                                </div>
+                                <div>
+                                    <SmallInputAndLabel pos="left" label="Location" id="location" name="location" value={props.values.location} onChange={props.handleChange} />
+                                    {props.errors.location && <Feedback>{props.errors.location}</Feedback>}
+                                </div>
                             </SecondRow>
 
+                            {/* <ThirdRow>
+                            </ThirdRow> */}
+
                             <ThirdRow>
-                                <CheckboxAndLabel label="Pets" id="pets" name="pets" value={props.values.pets} onChange={props.handleChange} />
-                                {props.errors.pets && <Feedback>{props.errors.pets}</Feedback>}
-                                <CheckboxAndLabel label="Parties" id="parties" name="parties" value={props.values.parties} onChange={props.handleChange} />
-                                {props.errors.parties && <Feedback>{props.errors.parties}</Feedback>}
+                                <InputAndLabel label="Personal features. Separtate with semicolon" id="features" name="features" value={props.values.features} onChange={props.handleChange} />
+                                {props.errors.features && <Feedback>{props.errors.features}</Feedback>}
                             </ThirdRow>
 
                             <ForthRow>
-                                <InputAndLabel label="Personal features. Separtate with semicolon" id="features" name="features" value={props.values.features} onChange={props.handleChange} />
-                                {props.errors.features && <Feedback>{props.errors.features}</Feedback>}
-                            </ForthRow>
-
-                            <FifthRow>
                                 <InputAndLabel label="Customs. Separtate with semicolon" id="customs" name="customs" value={props.values.customs} onChange={props.handleChange} />
                                 {props.errors.customs && <Feedback>{props.errors.customs}</Feedback>}
-                            </FifthRow>
+                            </ForthRow>
 
                             <ButtonWrapper>
                                 <SearchButton>Submit</SearchButton>
