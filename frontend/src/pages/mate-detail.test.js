e194653b42e3f91103138eb07c0a9be5f52d8219
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2020-10-15 19:28:07 +0200

    Fixed formatting

--- frontend/src/pages/mate-detail.test.js
+++ frontend/src/pages/mate-detail.test.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import {MateDetailPage}  from './mate-detail.page'
+import { MateDetailPage } from './mate-detail.page';
 
 it('checks if it passes props correctly', () => {
-  const component = shallow(<MateDetailPage/>);
-  component.setState({data: 
-    { id: 9, title: "Peaceful IT student", age: 21, 
-    location: "Grunwald", field_of_study: "Computer Science", 
-    features: "peaceful;quiet;gaming;cycling", 
-    customs: "no smoking;no partying;wakes up at 11-12;goes to bed 23-24",
-    phone: "123123123", owner: 5 }});
+  const component = shallow(<MateDetailPage />);
+  component.setState({
+    data:
+    {
+      id: 9, title: "Peaceful IT student", age: 21,
+      location: "Grunwald", field_of_study: "Computer Science",
+      features: "peaceful;quiet;gaming;cycling",
+      customs: "no smoking;no partying;wakes up at 11-12;goes to bed 23-24",
+      phone: "123123123", owner: 5
+    }
+  });
   expect(component.find('ContactBox').prop('phone')).toEqual('123123123');
 })
